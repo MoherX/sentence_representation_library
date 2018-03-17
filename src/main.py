c8503d0932b1bbf2bec88d1fc402ecfd0e186703
xiachongfeng <838711908@qq.com>
2018-03-17 10:14:03 +0800

    reconstruct
    reconstruct some codes and fix one bug

--- src/main.py
+++ src/main.py
@@ -20,12 +20,13 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
+
+from MyDataset import MyDataset
 from utils import flatten
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence
 import torch.utils.data as Data
 
-use_cuda = torch.cuda.is_available()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)-15s %(levelname)s: %(message)s')
 
@@ -67,8 +68,7 @@ def evaluate(instance_x, instance_y, model, batch_size):
     sum_all = len(gold)
     correct = map(cmp, gold, predict).count(0)
 
-    return correct*1.0 / sum_all
-
+    return correct * 1.0 / sum_all
 
 
 def main():
@@ -85,70 +85,75 @@ def main():
     cmd.add_argument("--kernel_size", help="kernel_size", type=str, default="3*4*5")
     cmd.add_argument("--kernel_num", help="kernel_num", type=str, default="100*100*100")
     cmd.add_argument("--l2", help="l2 norm", type=int, default=3)
-    cmd.add_argument("--encoder", help="options:[lstm, bilstm, gru, cnn, tri-lstm]", type = str, default='bilstm')
+    cmd.add_argument("--encoder", help="options:[lstm, bilstm, gru, cnn, tri-lstm]", type=str, default='bilstm')
+    cmd.add_argument("--gpu", action="store_true", help="use gpu")
 
     args = cmd.parse_args()
     torch.manual_seed(args.seed)  # fixed the seed
     random.seed(args.seed)
 
+    use_cuda = torch.cuda.is_available() and args.gpu  # gpu use
+
     batch_size = args.batch_size
     logging.info("args:{0}".format(args))
+
     train_x, train_y, valid_x, valid_y, test_x, test_y = preprocess(args.data_dir)
 
     lang = Lang()
-    lang = generate_dict(lang, train_x)
-    word_size = lang.get_word_size()
+    lang.generate_dict(train_x)  # 构建train语料词典
+    word_size = lang.get_word_size()  # 获取词典大小
 
     logging.info("dict generated finished! dict size:{0}".format(word_size))
 
-    train_x_idx, train_y_idx = sentence_to_idx(lang, train_x, train_y)
-    valid_x_idx, valid_y_idx = sentence_to_idx(lang, valid_x, valid_y)
-    test_x_idx, test_y_idx = sentence_to_idx(lang, test_x, test_y)
+    # 根据句子获得句子对应id序列，以及获取label对应id
+    train_x_idx, train_y_idx = lang.sentence_to_idx2(train_x, train_y)
+    valid_x_idx, valid_y_idx = lang.sentence_to_idx2(valid_x, valid_y)
+    test_x_idx, test_y_idx = lang.sentence_to_idx2(test_x, test_y)
 
-    logging.info('train size:{0}, valid size:{1}, test size:{2}'.format(len(train_x_idx), len(valid_x_idx), len(test_x_idx)))
-    lst = list(range(len(train_x_idx)))
+    logging.info(
+            'train size:{0}, valid size:{1}, test size:{2}'.format(len(train_x_idx), len(valid_x_idx), len(test_x_idx)))
 
-    model = Model(args, args.input_size, args.hidden_size, 5, word_size, args.embedding_size, args.dropout)  # control module
-
+    model = Model(args, args.input_size, args.hidden_size, 5, word_size, args.embedding_size,
+                  args.dropout)  # control module
 
     if use_cuda:
         model = model.cuda()
 
-
     dataset = MyDataset(train_x_idx, train_y_idx)
 
-    dataloader = Data.DataLoader(dataset = dataset,
-                                 batch_size = args.batch_size,
+    dataloader = Data.DataLoader(dataset=dataset,
+                                 batch_size=args.batch_size,
                                  shuffle=True,
-                                 collate_fn=collate_batch)
-
+                                 collate_fn=collate_batch)  # self-defined collate function
 
     # 待会可以写个程序画出loss的曲线
     best_valid_acc = 0.0
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    model.train()
     for epoch in range(args.max_epoch):
-        Round_loss = 0
+        round_loss = 0
         logging.info("epoch:{0} begins!".format(epoch))
-        for step, (batch_instance_x, batch_instance_y) in enumerate(dataloader): # 这里只是按照之前的进行自动shuffle，没有其它的要求，出来进行padding了和转换为Variable了
-            optimizer.zero_grad()
+        for step, (batch_instance_x, batch_instance_y) in enumerate(
+                dataloader):  # 这里只是按照之前的进行自动shuffle，没有其它的要求，出来进行padding了和转换为Variable了
 
-            model.train()
-
+            optimizer.zero_grad()  # 梯度清零
             loss = model.forward(batch_instance_x, batch_instance_y)  # 送进去forward的只是原始的idx表示，还没有padding
+            loss.backward()  # 反向传播
+            optimizer.step()  # 参数更新
+            round_loss += loss
 
-            loss.backward()
-            optimizer.step()
-            Round_loss += loss
-        logging.info("epoch:{0} loss:{1}".format(epoch, Round_loss.data[0]))
+        logging.info("epoch:{0} loss:{1}".format(epoch, round_loss.data[0]))
 
         valid_acc = evaluate(valid_x_idx, valid_y_idx, model, batch_size)
         logging.info("valid_acc = {0}".format(valid_acc))
 
-        if(valid_acc > best_valid_acc):
+        if valid_acc > best_valid_acc:
             best_valid_acc = valid_acc
             test_acc = evaluate(test_x_idx, test_y_idx, model, batch_size)  # 在测试集上进行测试
             # save model
-            logging.info("epoch:{0} New Record! valid_accuracy:{1}, test_accuracy:{2}".format(epoch, valid_acc, test_acc))
+            logging.info(
+                "epoch:{0} New Record! valid_accuracy:{1}, test_accuracy:{2}".format(epoch, valid_acc, test_acc))
 
     # finally, we evaluate valid and test dataset accuracy
     valid_acc = evaluate(valid_x_idx, valid_y_idx, model, batch_size)
@@ -159,6 +164,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
--- src/module.py
+++ src/module.py
@@ -17,7 +17,9 @@ import random
 
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
 use_cuda = torch.cuda.is_available()
+
 
 class Model(nn.Module):
     def __init__(self, args, input_size_, hidden_size_, output_size, vocal_size, embedding_size, dropout):
@@ -26,17 +28,18 @@ class Model(nn.Module):
         self.hidden_size = hidden_size_
         self.output_size = output_size
         self.vocal_size = vocal_size
-        self.embedding_size =embedding_size
+        self.embedding_size = embedding_size
         self.args = args
 
         if args.encoder == 'lstm':
-            self.encoder = lstm_model(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size, self.embedding_size, dropout)
+            self.encoder = LstmModel(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size,
+                                     self.embedding_size, dropout)
         elif args.encoder == 'bilstm':
-            self.encoder = bilstm_model(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size,
-                                      self.embedding_size, dropout)
+            self.encoder = BilstmModel(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size,
+                                       self.embedding_size, dropout)
         elif args.encoder == 'cnn':
-            self.encoder = cnn_model(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size,self.embedding_size, dropout)
-
+            self.encoder = CnnModel(self.args, self.input_size, self.hidden_size, self.output_size, self.vocal_size,
+                                    self.embedding_size, dropout)
 
     def forward(self, input_x, input_y):
         """
@@ -48,9 +51,9 @@ class Model(nn.Module):
         return self.encoder.forward(input_x, input_y)  # interface， implementated by every people
 
 
-class lstm_model(nn.Module):
+class LstmModel(nn.Module):
     def __init__(self, args, input_size_, hidden_size_, output_size, vocal_size, embedding_size, dropout):
-        super(lstm_model, self).__init__()
+        super(LstmModel, self).__init__()
 
         self.input_size = input_size_
         self.embedding_size = embedding_size
@@ -65,14 +68,14 @@ class lstm_model(nn.Module):
         self.NLLoss = nn.NLLLoss()
         self.dropout = nn.Dropout(dropout)
         self.softmax = nn.LogSoftmax()
-        self.lstm  = nn.LSTM(input_size = self.input_size,
-                             hidden_size = self.hidden_size,
-                             batch_first=True,
-                             dropout = dropout)
+        self.lstm = nn.LSTM(input_size=self.input_size,
+                            hidden_size=self.hidden_size,
+                            batch_first=True,
+                            dropout=dropout)
 
         self.w_i_in, self.w_i_on = self.lstm.all_weights[0][0].size()
         self.w_h_in, self.w_h_on = self.lstm.all_weights[0][1].size()
-        self.lstm.all_weights[0][0] = Parameter(torch.randn(self.w_i_in, self.w_i_on)) * np.sqrt(2./ self.w_i_on)
+        self.lstm.all_weights[0][0] = Parameter(torch.randn(self.w_i_in, self.w_i_on)) * np.sqrt(2. / self.w_i_on)
         self.lstm.all_weights[0][1] = Parameter(torch.randn(self.w_h_in, self.w_h_on)) * np.sqrt(2. / self.w_h_on)
 
     def forward(self, input_x, input_y):
@@ -96,29 +99,28 @@ class lstm_model(nn.Module):
 
         input = input_x.squeeze(1)
 
-        embed_input_x = self.embedding(input) # embed_intput_x: (b_s, m_l, em_s)
+        embed_input_x = self.embedding(input)  # embed_intput_x: (b_s, m_l, em_s)
         embed_input_x = self.dropout(embed_input_x)
-
 
         embed_input_x_packed = pack_padded_sequence(embed_input_x, sentence_lens, batch_first=True)
         encoder_outputs_packed, (h_last, c_last) = self.lstm(embed_input_x_packed)
         encoder_outputs, _ = pad_packed_sequence(encoder_outputs_packed, batch_first=True)
 
-
         predict = self.linear(h_last)  # predict: [1, b_s, o_s]
-        predict = self.softmax(predict.squeeze(0)) # predict.squeeze(0) [b_s, o_s]
+        predict = self.softmax(predict.squeeze(0))  # predict.squeeze(0) [b_s, o_s]
 
         loss = self.NLLoss(predict, input_y)
 
-        if(self.training):  # if it is in training module
+        if self.training:  # if it is in training module
             return loss
         else:
             value, index = torch.max(predict, 1)
             return index  # outsize, cal the acc
 
-class bilstm_model(nn.Module):
+
+class BilstmModel(nn.Module):
     def __init__(self, args, input_size_, hidden_size_, output_size, vocal_size, embedding_size, dropout):
-        super(bilstm_model, self).__init__()
+        super(BilstmModel, self).__init__()
 
         self.input_size = input_size_
         self.embedding_size = embedding_size
@@ -136,7 +138,6 @@ class bilstm_model(nn.Module):
                             batch_first=True,
                             dropout=dropout,
                             bidirectional=True)
-
 
     def forward(self, input_x, input_y):
         """
@@ -163,87 +164,82 @@ class bilstm_model(nn.Module):
         embed_input_x = self.dropout(embed_input_x)
 
         encoder_outputs, (h_last, c_last) = self.lstm(embed_input_x)
-        h_last = torch.cat(h_last, 1)
+        h_last = torch.cat((h_last[0], h_last[1]), 1)
 
         predict = self.linear(h_last)  # predict: [1, b_s, o_s]
         predict = self.softmax(predict.squeeze(0))  # predict.squeeze(0) [b_s, o_s]
 
         loss = self.NLLoss(predict, input_y)
 
-        if (self.training):  # if it is in training module
+        if self.training:  # if it is in training module
             return loss
         else:
             value, index = torch.max(predict, 1)
             return index  # outsize, cal the acc
 
 
+class CnnModel(nn.Module):
 
-class cnn_model(nn.Module):
+    def __init__(self, args, input_size_, hidden_size_, output_size, vocal_size, embedding_size, dropout):
+        super(CnnModel, self).__init__()
 
-	def __init__(self, args, input_size_, hidden_size_, output_size, vocal_size, embedding_size, dropout):
-		super(cnn_model, self).__init__()
+        self.input_size = input_size_
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size_
+        self.output_size = output_size
+        self.vocal_size = vocal_size
+        self.seed = args.seed
+        self.embedding = nn.Embedding(self.vocal_size, self.embedding_size)
+        self.NLLoss = nn.NLLLoss()
+        self.dropout = nn.Dropout(dropout)
+        self.softmax = nn.LogSoftmax()
 
-		self.input_size = input_size_
-		self.embedding_size = embedding_size
-		self.hidden_size = hidden_size_
-		self.output_size = output_size
-		self.vocal_size = vocal_size
-		self.seed = args.seed
-		self.embedding = nn.Embedding(self.vocal_size, self.embedding_size)
-		self.NLLoss = nn.NLLLoss()
-		self.dropout = nn.Dropout(dropout)
-		self.softmax = nn.LogSoftmax()
-		
-		self.l2 = args.l2
-		self.kernel_size = [int(size) for size in args.kernel_size.split("*")]
-		self.kernel_num = [int(num) for num in args.kernel_num.split("*")]
-		nums = 0
-		for n in self.kernel_num:
-			nums += n		
-		self.linear = nn.Linear(nums, self.output_size)
-		self.convs = nn.ModuleList([nn.Conv2d(1,num,(size,self.embedding_size)) for (size,num) in zip(self.kernel_size,self.kernel_num)])
-		
+        self.l2 = args.l2
+        self.kernel_size = [int(size) for size in args.kernel_size.split("*")]
+        self.kernel_num = [int(num) for num in args.kernel_num.split("*")]
+        nums = 0
+        for n in self.kernel_num:
+            nums += n
+        self.linear = nn.Linear(nums, self.output_size)
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(1, num, (size, self.embedding_size)) for (size, num) in zip(self.kernel_size, self.kernel_num)])
 
-	def forward(self, input_x, input_y):
-		input_x, input_y, sentence_lens = padding(input_x, input_y)
-		max_len = len(input_x[0])
-		self.poolings = nn.ModuleList([nn.MaxPool1d(max_len - size + 1,1) for size in self.kernel_size ])#the output of each pooling layer is a number
+    def forward(self, input_x, input_y):
+        input_x, input_y, sentence_lens = padding(input_x, input_y)
+        max_len = len(input_x[0])
+        self.poolings = nn.ModuleList([nn.MaxPool1d(max_len - size + 1, 1) for size in
+                                       self.kernel_size])  # the output of each pooling layer is a number
 
-		if use_cuda:
-			input_x = Variable(torch.LongTensor(input_x)).cuda()
-			input_y = Variable(torch.LongTensor(input_y)).cuda()
-		else:
-			input_x = Variable(torch.LongTensor(input_x))
-			input_y = Variable(torch.LongTensor(input_y))
+        if use_cuda:
+            input_x = Variable(torch.LongTensor(input_x)).cuda()
+            input_y = Variable(torch.LongTensor(input_y)).cuda()
+        else:
+            input_x = Variable(torch.LongTensor(input_x))
+            input_y = Variable(torch.LongTensor(input_y))
 
-		input = input_x.squeeze(1)
+        input = input_x.squeeze(1)
 
-		embed_input_x = self.embedding(input)  # embed_intput_x: (b_s, m_l, em_s)
-		embed_input_x = self.dropout(embed_input_x)
-		embed_input_x = embed_input_x.view(embed_input_x.size(0),1,-1,embed_input_x.size(2))
+        embed_input_x = self.embedding(input)  # embed_intput_x: (b_s, m_l, em_s)
+        embed_input_x = self.dropout(embed_input_x)
+        embed_input_x = embed_input_x.view(embed_input_x.size(0), 1, -1, embed_input_x.size(2))
 
-		parts = []#example:[3,4,5] [100,100,100] the dims of data though pooling layer is 100 + 100 + 100 = 300    
-		for (conv,pooling) in zip(self.convs,self.poolings):
-			parts.append(pooling(conv(embed_input_x).squeeze()).view(input_x.size(0),-1))
-		x = F.relu(torch.cat(parts,1))
+        parts = []  # example:[3,4,5] [100,100,100] the dims of data though pooling layer is 100 + 100 + 100 = 300
+        for (conv, pooling) in zip(self.convs, self.poolings):
+            parts.append(pooling(conv(embed_input_x).squeeze()).view(input_x.size(0), -1))
+        x = F.relu(torch.cat(parts, 1))
 
-		# make sure the l2 norm of w less than l2
-		w = torch.mul(self.linear.weight,self.linear.weight).sum().data[0]
-		if  w > self.l2*self.l2:
-			x = torch.mul(x.weight,math.sqrt(self.l2*self.l2*1.0/w))
+        # make sure the l2 norm of w less than l2
+        w = torch.mul(self.linear.weight, self.linear.weight).sum().data[0]
+        if w > self.l2 * self.l2:
+            x = torch.mul(x.weight, math.sqrt(self.l2 * self.l2 * 1.0 / w))
 
-		predict = self.linear(x)  # predict: [1, b_s, o_s]
-		predict = self.softmax(predict.squeeze(0))  # predict.squeeze(0) [b_s, o_s]
+        predict = self.linear(x)  # predict: [1, b_s, o_s]
+        predict = self.softmax(predict.squeeze(0))  # predict.squeeze(0) [b_s, o_s]
 
-		loss = self.NLLoss(predict, input_y)
+        loss = self.NLLoss(predict, input_y)
 
-		if (self.training):  # if it is in training module
-			return loss
-		else:
-			value, index = torch.max(predict, 1)
-			return index  # outsize, cal the acc
-				
-
-
-
-
+        if self.training:  # if it is in training module
+            return loss
+        else:
+            value, index = torch.max(predict, 1)
+            return index  # outsize, cal the acc
